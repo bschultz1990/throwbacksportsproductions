7c12b70011c3df3efdde4dffd8577f1aae5b862f
= <=>
2023-05-19 21:43:05 -0500

    Simplify CSS and focus on a mobile-first layout.

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const footer = document.getElementById('footer');
+const footer = document.getElementById("footer");
 const currentYear = new Date().getFullYear();
 
 footer.innerText = `Copyright © ${currentYear} Throwback Sports Productions`;
